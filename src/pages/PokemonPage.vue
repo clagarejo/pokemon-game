8f62772c920b45ae2b7844ccc497d24dc56d5f56
Yan Lagarejo <clagarejo89@gmail.com>
2024-03-02 20:10:17 -0500

    Pokemon Game

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,82 @@
+<template>
+  <h1 v-if="!pokemon">Espere por favor...</h1>
+
+  <div v-else>
+    <h1>¿Quién es este pokémon?</h1>
+
+    <PokemonPicture :pokemon-id="pokemon.id" :show-pokemon="showPokemon" />
+
+    <PokemonOptions :pokemons="pokemonArr" @selection-pokemon="checkAnswer" />
+
+    <template v-if="showAnswer">
+      <h2 class="fade-in">{{ message }}</h2>
+      <button
+        @click="newGame"
+        class="btn"
+        :class="{
+          'btn-success': idPokemon === pokemon.id,
+          'btn-danger': idPokemon !== pokemon.id,
+          'py-3': true,
+          'px-5': true,
+        }"
+      >
+        {{ idPokemon === pokemon.id ? "Nuevo Juego" : "Intenta de nuevo" }}
+      </button>
+    </template>
+  </div>
+</template>
+
+<script>
+import PokemonOptions from "@/components/PokemonOptions";
+import PokemonPicture from "@/components/PokemonPicture";
+
+import getPokemonOptions from "@/helpers/getPokemonOptions";
+
+export default {
+  components: { PokemonOptions, PokemonPicture },
+  data() {
+    return {
+      pokemonArr: [],
+      pokemon: null,
+      showPokemon: false,
+      showAnswer: false,
+      message: "",
+      idPokemon: null,
+    };
+  },
+  methods: {
+    async mixPokemonArray() {
+      this.pokemonArr = await getPokemonOptions();
+
+      const rndInt = Math.floor(Math.random() * 4);
+      this.pokemon = this.pokemonArr[rndInt];
+    },
+    checkAnswer(selectedId) {
+      this.idPokemon = selectedId;
+      this.showPokemon = true;
+      this.showAnswer = true;
+
+      if (selectedId === this.pokemon.id) {
+        this.message = `Correcto, ${this.pokemon.name}`;
+      } else {
+        this.message = `Oops, era ${this.pokemon.name}`;
+      }
+    },
+    newGame() {
+      this.showPokemon = false;
+      this.showAnswer = false;
+      this.pokemonArr = [];
+      this.pokemon = null;
+      this.mixPokemonArray();
+    },
+  },
+  mounted() {
+    this.mixPokemonArray();
+  },
+};
+</script>
+
+<style scoped>
+.new-game {
+}
+</style>
